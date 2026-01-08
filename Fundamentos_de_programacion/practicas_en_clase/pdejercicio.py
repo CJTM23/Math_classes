f6f2805a4a3675daba1aed812beb4d27e6a4128d
CltControl <chrimira@espol.edu.ec>
2026-01-08 12:58:23 -0500

    Pandas

--- Fundamentos_de_programacion/practicas_en_clase/pdejercicio.py
+++ Fundamentos_de_programacion/practicas_en_clase/pdejercicio.py
@@ -20,4 +20,30 @@ print(df.columns)
 # df=df.sort_values("monthly_salary",ascending=False)
 # df=df.head(5)
 # df=(df["employee_name"].str[0]== "A") & (df["employee_name"].str.endswith("a"))
-print(df["employee_name"].value_counts())
+#print(df["employee_name"].value_counts())
+
+##eliminar datos
+# sBooleana=df["job_level"].isna()
+# df=df[sBooleana]
+# print(df)
+# df=df[~sBooleana]
+# df=df.dropna()
+
+##eliminar dato y reemplazarlo
+
+# df.loc[df["job_level"].isna(),"job_level"] ="sin Titulo"
+# print(df[['employee_name', 'department', 'job_level']])
+
+# empleados con 45,23,57,35,44
+# sBooleana=df["age"].isin([45,23,57,35,44])
+# print(df[sBooleana])
+
+#empleados sin repetir nombres
+nombres=df["employee_name"].unique()
+print(nombres.size)
+
+#top
+print(df["performance_score"].nlargest(3))
+print(df.nlargest(5,columns=["performance_score"]))
+print(df.nlargest(5,columns=["performance_score","monthly_salary"]))
+print(df.nsmallest(10,columns=["performance_score","monthly_salary"]))
